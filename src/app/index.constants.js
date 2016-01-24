937856eea400687550f887050b99fc2d1b845daa
Gareth Parker <rpgherogaz@gmail.com>
2016-01-24 17:04:59 +0000

    Init
    Init Commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('tableTopr')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
